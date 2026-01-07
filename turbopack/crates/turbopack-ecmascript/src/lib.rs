acc72e8c29196288ac6cc4e45f87955e279df8d2
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-01-07 13:25:45 +0100

    Turbopack: remove sourcemapping comments

--- turbopack/crates/turbopack-ecmascript/src/lib.rs
+++ turbopack/crates/turbopack-ecmascript/src/lib.rs
@@ -2044,6 +2044,7 @@ async fn with_consumed_parse_result<T>(
                     globals,
                     eval_context,
                     comments,
+                    ..
                 }) => (
                     program.take(),
                     &*source_map,
@@ -2069,6 +2070,7 @@ async fn with_consumed_parse_result<T>(
                         globals,
                         eval_context,
                         comments,
+                        ..
                     } = &**parsed
                     else {
                         unreachable!();
