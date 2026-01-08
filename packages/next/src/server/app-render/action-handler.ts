c57c0bc719cf3f5da8f819c71295437244bb95db
Jiachi Liu <inbox@huozhi.im>
2026-01-08 16:35:24 +0100

    normalize proj path

--- packages/next/src/server/app-render/action-handler.ts
+++ packages/next/src/server/app-render/action-handler.ts
@@ -1099,7 +1099,9 @@ export async function handleAction({
             } else {
               functionName = actionInfo.exportedName || '<inline action>'
             }
-            const projectDir = ctx.renderOpts.dir || process.cwd()
+            const projectDir =
+              ctx.renderOpts.dir ||
+              (process.env.NEXT_RUNTIME === 'edge' ? '' : process.cwd())
             const filename = normalizeFilePath(projectDir, actionInfo.filename)
 
             // Build location string with line:col if available
